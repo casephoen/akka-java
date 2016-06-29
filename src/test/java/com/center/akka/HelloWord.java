2400f90141238e8097fd71f4a0068b35cb5f231e
lizhijun <lizj@topgame.com>
2016-06-29 14:47:23 +0800

    hello world

--- src/test/java/com/center/akka/HelloWord.java
+++ src/test/java/com/center/akka/HelloWord.java
@@ -2,26 +2,26 @@ package com.center.akka;
 
 import scala.Option;
 import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 
 public class HelloWord extends UntypedActor {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		/*
-		 * ActorSystem system = ActorSystem.create("akka"); ActorRef rcActor =
-		 * system.actorOf(Props.create(HelloWord.class), "helloWorld");
-		 * rcActor.tell("hi", ActorRef.noSender()); // Thread.sleep(3000); //
-		 * Actor树 ActorSelection as = system.actorSelection("/user/helloWorld");
-		 * as.tell("hello", ActorRef.noSender());
-		 */
+		ActorSystem system = ActorSystem.create("akka");
+		ActorRef rcActor = system.actorOf(Props.create(HelloWord.class), "helloWorld");
+		rcActor.tell("hi", ActorRef.noSender()); 
+		Thread.sleep(6000);
+		ActorSelection as = system.actorSelection("/user/helloWorld");
+		as.tell("hello", ActorRef.noSender());
 
-		ActorSystem system = ActorSystem.create("demo1");
+//		ActorSystem system = ActorSystem.create("demo1");
 		ActorRef actor1 = system.actorOf(Props.create(Actor01.class));
 		ActorRef actor2 = system.actorOf(Props.create(Actor02.class));
 		actor2.tell("hello akka!!", actor1);
-		system.shutdown();//
+		system.shutdown();
 
 	}
 
@@ -52,7 +52,7 @@ public class HelloWord extends UntypedActor {
 	public void onReceive(Object msg) {
 		if (msg.toString().equals("hello")) {
 			System.out.println("----" + msg);
-			// getContext().stop(getSelf());
+			getContext().stop(getSelf());
 		} else {
 			// 导致崩溃，actor会自动重启
 			System.out.println(3 / 0);
